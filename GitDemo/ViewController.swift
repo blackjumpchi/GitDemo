91ffd530e4de820cfc4d5437770cabb0bec65b33
Tzu Han Li <cathagice@gmail.com>
2018-02-01 15:14:03 +0800

    print 2

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         print("Git Test")
+        print("Git Test")
+
     }
 
     override func didReceiveMemoryWarning() {
